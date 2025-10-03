c2d91abee0e5d393c1f382a3d1ce27450d0e945c
joynm08 <joynm08@gmail.com>
2025-10-03 18:00:55 +0900

    Clean up all files for fresh start

--- py/0929.py
+++
@@ -1,7 +0,0 @@
-print('# if 조건문에 0넣기')
-if 0: print("0은 true로 변환 됩니다")
-else : 
-    print("0은 false로 치환 됩니다")
-print()    
-if "" : print("빈문자열은 true로 변환됩니다")
-else : print("빈문자열은 false 로 변환됩니다")python --version
